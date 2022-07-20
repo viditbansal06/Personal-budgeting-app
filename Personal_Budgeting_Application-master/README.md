df3e1c49ca3b80b161e34bc4de80c0bc82034665
Vidit <viditbansal0106@gmail.com>
2022-07-21 04:20:56 +0530

    files added

---
+++ Personal_Budgeting_Application-master/README.md
@@ -0,0 +1,37 @@
+# Personal_Budgeting_Application
+Budgeting is creating a plan to spend your money. Good budgeting is spending less than
+you are earning as you plan for your financial goals. Budgeting is the fundamental step
+in achieving financial literacy, and by extension, reaching financial security and freedom.
+Budgeting is the process of creating a plan to spend and invest your hard earned money
+wisely to meet your personal and financial goals in life. It should not be a mathematical
+exercise that we think we have to endure; rather, it is the result of self-assessment of our
+relationship with money and a necessary road map to steer us toward a higher standard
+and quality of living.
+# Abstract
+Budgeting is the most basic and effective tool for managing our monthly income. But
+most people avoid doing it because they think keeping a record of budget or expense is
+a hectic task. Recording monthly expenses on paper are very difficult as modification,
+removal, and searching are not easy on paper. Similarly using any which run on our
+computers like MS excel or any other tool is also clumsy. Because we will have to note all
+expenses on paper then later record them all on the tool installed on a computer. Better
+solutions is to use mobile applications to keep a record of every expense easily at any
+location. In this project, the task is to develop an Android-based application that will
+be used to manage personal budget.
+# Tools Used
+• Programming Language : Android
+#
+• User Interface : XML
+#
+• Database : Firebase
+#
+• IDE : Android Studio
+# Application
+Personal Budgeting Application is exactly how much money you have and where it’s
+being spent. A budget can help you find ways to save money and plan for the future.
+Since budgeting allows you to create a spending plan for your money, it ensures that you
+will always have enough money for the things you need and the things that are important
+to you. Following a budget or spending plan will also keep you out of debt or help you
+work your way out of debt if you are currently in debt.
+Therefore, budgeting can be a way to improve your life. Debt management and other
+personal financial tasks are made easier if proper budgeting is in place.
+
---
+++ Personal_Budgeting_Application-master/app/src/main/java/com/example/personalbudgetingapp/MainActivity.java
@@ -0,0 +1,345 @@
+package com.example.personalbudgetingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.MutableDateTime;
+import org.joda.time.Weeks;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private CardView budgetCardView, todayCardView;
+
+    private TextView weekSpendingTv, budgetTv, todaySpendingTv, remainingBudgetTv, monthSpendingTv;
+
+    private ImageView todayAnlyBtnImageView, todayBtnImageView, budgetBtnImageView, monthBtnImageView, monthAnlyBtnImageView,historyBtnImageView,t;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference budgetRef, expensesRef, personalRef;
+    private String onlineUserID = "";
+
+    private int totalAmountMonth = 0;
+    private int totalAmountBudget = 0;
+    private int totalAmountBudgetB = 0;
+    private int totalAmountBudgetC = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Personal Budgeting App");
+
+        budgetTv = findViewById(R.id.budgetTv);
+        todaySpendingTv = findViewById(R.id.todayTv);
+        remainingBudgetTv = findViewById(R.id.savingsTv);
+        monthSpendingTv = findViewById(R.id.monthTv);
+        weekSpendingTv = findViewById(R.id.weekTv);
+
+        mAuth = FirebaseAuth.getInstance();
+        onlineUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        budgetRef = FirebaseDatabase.getInstance().getReference("budget").child(onlineUserID);
+        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        personalRef = FirebaseDatabase.getInstance().getReference("personal").child(onlineUserID);
+
+        todayAnlyBtnImageView = findViewById(R.id.todayAnlyBtnImageView);
+        todayBtnImageView = findViewById(R.id.todayBtnImageView);
+        budgetBtnImageView = findViewById(R.id.budgetBtnImageView);
+        monthBtnImageView = findViewById(R.id.monthBtnImageView);
+        historyBtnImageView = findViewById(R.id.historyBtnImageView);
+
+        todayCardView = findViewById(R.id.todayCardView);
+        monthAnlyBtnImageView = findViewById(R.id.monthAnlyBtnImageView);
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        budgetBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historyBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        todayBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        todayAnlyBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DailyAnalyticsActivity.class);
+                intent.putExtra("type", "week");
+                startActivity(intent);
+
+            }
+        });
+
+        monthBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                intent.putExtra("type", "month");
+                startActivity(intent);
+            }
+        });
+
+        monthAnlyBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MonthlyAnalyticsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        budgetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists() && snapshot.getChildrenCount() > 0) {
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        Map<String, Object> map = (Map<String, Object>) ds.getValue();
+                        Object total = map.get("amount");
+                        int pTotal = Integer.parseInt(String.valueOf(total));
+                        totalAmountBudgetB += pTotal;
+                    }
+                    totalAmountBudgetC = totalAmountBudgetB;
+                    personalRef.child("budget").setValue(totalAmountBudgetC);
+                } else {
+                    personalRef.child("budget").setValue(0);
+                    Toast.makeText(MainActivity.this, "Please Set a BUDGET ", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        getBudgetAmount();
+        getTodaySpentAmount();
+        getWeekSpentAmount();
+        getMonthSpentAmount();
+        getSavings();
+    }
+    private void getBudgetAmount() {
+        budgetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists() && snapshot.getChildrenCount()>0){
+                    for (DataSnapshot ds :  snapshot.getChildren()){
+                        Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                        Object total = map.get("amount");
+                        int pTotal = Integer.parseInt(String.valueOf(total));
+                        totalAmountBudget+=pTotal;
+                        budgetTv.setText("$ "+String.valueOf(totalAmountBudget));
+                    }
+                }else {
+                    totalAmountBudget=0;
+                    budgetTv.setText("$ "+String.valueOf(0));
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getTodaySpentAmount(){
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("date").equalTo(date);
+        ((Query) query).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    todaySpendingTv.setText("$ "+ totalAmount);
+                }
+                personalRef.child("today").setValue(totalAmount);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getMonthSpentAmount(){
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0); //Set to Epoch time
+        DateTime now = new DateTime();
+        Months months = Months.monthsBetween(epoch, now);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("month").equalTo(months.getMonths());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    monthSpendingTv.setText("$ "+ totalAmount);
+
+                }
+                personalRef.child("month").setValue(totalAmount);
+                totalAmountMonth = totalAmount;
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getWeekSpentAmount(){
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0); //Set to Epoch time
+        DateTime now = new DateTime();
+        Weeks weeks = Weeks.weeksBetween(epoch, now);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("week").equalTo(weeks.getWeeks());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    weekSpendingTv.setText("$ "+ totalAmount);
+                }
+                personalRef.child("week").setValue(totalAmount);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void getSavings(){
+        personalRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull final DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    int budget;
+                    if (snapshot.hasChild("budget")) {
+                        budget = Integer.parseInt(snapshot.child("budget").getValue().toString());
+                    } else {
+                        budget = 0;
+                    }
+                    int monthSpending;
+                    if (snapshot.hasChild("month")) {
+                        monthSpending = Integer.parseInt(Objects.requireNonNull(snapshot.child("month").getValue().toString()));
+                    } else {
+                        monthSpending = 0;
+                    }
+
+                    int savings = budget - monthSpending;
+                    remainingBudgetTv.setText("$ " + savings);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.account){
+            Intent intent  = new Intent(MainActivity.this, AccountActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
